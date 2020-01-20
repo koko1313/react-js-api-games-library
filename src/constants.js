926d35c0acd436989b6679b20582e59c318e1ad5
koko1313 <kaloyanvellichkov@gmail.com>
2020-01-20 19:17:35 +0200

    removed unused code

--- src/constants.js
+++ src/constants.js
@@ -1,31 +1,3 @@
 export default {
-
     baseURL: "https://api.rawg.io/api/",
-
-    allGenres: () => {
-        return [
-            {
-                id: 1,
-                name: "Horror",
-            },
-            {
-                id: 2,
-                name: "Action",
-            },
-        ];
-    },
-
-    allPlatforms: () => {
-        return [
-            {
-                id: 1,
-                name: "PC",
-            },
-            {
-                id: 2,
-                name: "XBOX",
-            },
-        ];
-    }
-    
 }
