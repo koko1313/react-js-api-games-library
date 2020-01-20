8cee678332db0796ee5343b5d06bb144f1d7fd91
koko1313 <kaloyanvellichkov@gmail.com>
2020-01-20 21:21:35 +0200

    footed added

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="container">
+            Библиотека с игри 2020
+        </div>
+    );
+};
+
+export default Footer;
