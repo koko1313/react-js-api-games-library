04156de1c3b2e4cecc6d20b5122551e93f3b0793
koko1313 <kaloyanvellichkov@gmail.com>
2020-01-21 14:44:40 +0200

    style updated

--- src/components/GamesList/index.js
+++ src/components/GamesList/index.js
@@ -7,7 +7,7 @@ class GamesList extends Component {
         return <>
             <div className="row game-result py-3 my-3">
                 <div className="col-md-4">
-                    <img className="img-fluid" src="https://media.rawg.io/media/games/b11/b115b2bc6a5957a917bc7601f4abdda2.jpg" alt="game-slug"/>
+                    <img className="img-fluid mb-3 mb-md-0" src="https://media.rawg.io/media/games/b11/b115b2bc6a5957a917bc7601f4abdda2.jpg" alt="game-slug"/>
                 </div>
                 <div className="col-md">
                     <h3>Grand Theft Auto V</h3>
