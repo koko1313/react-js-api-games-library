72016593cfafc3ee4ce3dec770906669ddbc87bd
koko1313 <kaloyanvellichkov@gmail.com>
2020-01-23 00:27:23 +0200

    refactor

--- src/components/GamesList/GameResultDesign/BaseResult.js
+++ src/components/GamesList/GameResultDesign/BaseResult.js
@@ -10,18 +10,16 @@ class BaseResult extends Component {
         let platforms = [];
         game.parent_platforms.map((platform) => {
             platforms.push(platform.platform.name);
-        }, () => {
-            return platforms.join(", ");
         });
+        return platforms.join(", ");
     }
 
     getGenreNamesAsString = (game) => {
         let genres = [];
         game.genres.map((genre) => {
             genres.push(genre.name);
-        }, () => {
-            return genres.join(", ");
         });
+        return genres.join(", ");
     }
 
     render() {
